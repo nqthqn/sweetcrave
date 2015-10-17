f155d079ef6f1c3ea1d19938a7c23f0eccfed6c5
Nate Nichols <nathann@ori.org>
2015-10-17 14:11:27 -0700

    sending to other computer

--- CRAVECRUSH/CraveCrush.py
+++ CRAVECRUSH/CraveCrush.py
@@ -59,7 +59,7 @@ win = visual.Window(monSize, fullscr=fullscr,
 # instruction Stims
 instruction1_text = visual.TextStim(win, pos=(0, 0), text="You will see pictures of a chocolate milkshake that will be followed by tastes of milkshake. At certain points, you will be asked for a rating from 1 to 5. \n\nUse the button box to rate your craving for chocolate milkshake after seeing the picture. \n\nPress a button to continue.")
 instruction2_text = visual.TextStim(win, pos=(0, 0), text="When you are instructed to administer the dose of Crave Crush or placebo, move slowly and wait until the last five seconds to put it in your mouth. \n\nPress a button to continue.")
-instruction3_text = visual.TextStim(win, pos=(0, 0), text="Remember to follow the instructions carefully. \n\nPress a button when you are ready for the scan to begin.")
+instruction3_text = visual.TextStim(win, pos=(0, 0), text="Remember to follow the instructions carefully. \n\nPress a button to continue.")
 
 # Other Stims
 fixation_text = visual.TextStim(win, text='+', pos=(0, 0), height=2)
@@ -69,12 +69,13 @@ pumping_text = visual.TextStim(win, text='Pumping...', pos=(0, 0))
 scan_trigger_text = visual.TextStim(win, text='Waiting for scan trigger...', pos=(0, 0))
 swallow_text = visual.TextStim(win, text='Swallow', pos=(0, 0))
 milkshake_image = visual.ImageStim(win, image='Milkshake.jpg')
-crave_rating_scale = visual.RatingScale(win=win, name='crave_rating', marker=u'triangle', size=1.0, pos=[0.0, -0.5], low=1, high=5, labels=[u''], scale=u'Rate your craving from 1 - 5')
+
+crave_rating_scale = visual.RatingScale(win=win, name='crave_rating', marker=u'triangle', size=1.0, pos=[0.0, -0.7], low=1, high=5, labels=[u''], scale=u'Rate your craving from 1 - 5')
 
 def show_instruction(instrStim):
     # shows an instruction until a key is hit.
     while True:
-        instruction1.draw()
+        instrStim.draw()
         win.flip()
         if len(event.getKeys()) > 0:
             break
@@ -90,7 +91,7 @@ def run_block():
     # Pump test
     ser.write('run01\r')
     while True:
-        pumping.draw()
+        pumping_text.draw()
         win.flip()
         if 'c' in event.waitKeys():
             break
@@ -105,6 +106,7 @@ def run_block():
     while True:
         scan_trigger_text.draw()
         win.flip()
+        # keycode 39
         if 'apostrophe' in event.waitKeys():
             break
         event.clearEvents()
@@ -114,12 +116,10 @@ def run_block():
 
         show_stim(fixation_text, 10)  # 10 sec blank screen with fixation cross
         show_stim(milkshake_image, 10)  # 10 sec milkshake image
-        # TODO: 5 sec milkshake image with craving scale below, participants are asked to rate their craving for the milkshake on the button box 1-5
-        show_stim(crave_rating, 5) # add milkshake too
+        show_stim(crave_rating_scale, 5) # 5 sec milkshake image with craving scale below, participants are asked to rate their craving for the milkshake on the button box 1-5
         show_stim(fixation_text, 20) #  20 second fixation cross
-        #  5> Four cycles of taste delivery (10 sec each, screen that says 'taste delivery') and swallow (2 sec each, screen that says 'swallow')- total 48 sec
-        core.quit()
 
+        #  Four cycles of taste delivery (10 sec each, screen that says 'taste delivery') and swallow (2 sec each, screen that says 'swallow')- total 48 sec
         for cycle in range(4):
             ser.write('run01\r')
             for frame in range(60 * 10):
@@ -129,20 +129,17 @@ def run_block():
                 swallow.draw()
                 win.flip()
 
-        #  6> 20 second blank screen with fixation cross
-        for frame in range(60 * 10):
-            fixation_text.draw()
-            win.flip()
-        #  7> 10 sec milkshake image
-        for frame in range(60 * 10):
-            milkshake_image.draw()
-            win.flip()
+
+        show_stim(fixation_text, 20) #  20 second blank screen with fixation cross
+        show_stim(milkshake_image,10) #  10 sec milkshake image
+
         #  8> 5 sec milkshake pic with craving scale below
         for frame in range(60 * 5):
             milkshake_image.draw()
-            crave_rating.draw()
-            # TODO: get bbox input
+            crave_rating_scale.draw()
+            # TODO: get bbox input + fix bug with 2 bbox
             win.flip()
+
         #  9> 60 sec screen that counts down and tells participant to administer crave crush/placebo
         if cycle == 0:
             timer = core.CountdownTimer(60)
