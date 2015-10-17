7a8cd6181defba44579385e86b3022a4cc326aeb
Nate Nichols <nathann@ori.org>
2015-10-17 15:43:12 -0700

    sending to other computer

--- CRAVECRUSH/CraveCrush.py
+++ CRAVECRUSH/CraveCrush.py
@@ -30,14 +30,7 @@ time.sleep(1)
 pump_setup = ['VOL ML\r','TRGFT\r','AL 0\r','PF 0\r','BP 1\r','BP 1\r']
 
 # infuse 1mL@6mm, infuse .5mL@6mm, withdraw .7mL@15mm (max),
-pump_phases = ['dia26.59\r', 'phn01\r', 'funrat\r', 'rat6mm\r', 'vol1\r', 'dirinf\r', \
-'phn02\r', 'funrat\r', 'rat6mm\r', 'vol.5\r', 'dirinf\r', \
-'phn03\r', 'funrat\r', 'rat15mm\r', 'vol0.7\r', 'dirwdr\r', \
-'phn04\r', 'funstp\r', \
-'dia26.59\r', 'phn01\r', 'funrat\r', 'rat15mm\r', 'vol1.0\r', 'dirinf\r', \
-'phn02\r', 'funrat\r', 'rat7.5mm\r', 'vol.5\r', 'dirinf\r', \
-'phn03\r', 'funrat\r', 'rat15mm\r', 'vol1.0\r', 'dirwdr\r', \
-'phn04\r', 'funstp\r']
+pump_phases = ['dia26.59\r', 'phn01\r', 'funrat\r', 'rat6mm\r', 'vol1\r', 'dirinf\r', 'phn02\r', 'funrat\r', 'rat6mm\r', 'vol.5\r', 'dirinf\r', 'phn03\r', 'funrat\r', 'rat15mm\r', 'vol0.7\r', 'dirwdr\r', 'phn04\r', 'funstp\r', 'dia26.59\r', 'phn01\r', 'funrat\r', 'rat15mm\r', 'vol1.0\r', 'dirinf\r', 'phn02\r', 'funrat\r', 'rat7.5mm\r', 'vol.5\r', 'dirinf\r', 'phn03\r', 'funrat\r', 'rat15mm\r', 'vol1.0\r', 'dirwdr\r', 'phn04\r', 'funstp\r']
 
 for c in pump_setup:
     ser.write(c)
